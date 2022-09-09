0f1584213bfbcbf8c02366a0823fbd1ff3171b2a
obu <obu@DESKTOP-1GD9MPG>
2022-09-09 22:38:53 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+	string a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, A, B, C, D, E, F, G;
+	cout << "Spune versurile din Puya - fresh" << endl;
+	getline(cin, a);
+
+	if (a == "O luam de la cap din nou") {
+
+
+		cout << "Bravo, continua" << endl;
+		getline(cin, b);
+		if (b == "Stii ca sunt dilau lau lau") {
+			cout << "Bravo, continua" << endl;
+			getline(cin, c);
+			if (c == "Am la mine banii pe show") {
+				cout << "Bravo, continua" << endl;
+				getline(cin, d);
+				if (d == "Bagabontii in dreapta go go go") {
+					cout << "Bravo, continua" << endl;
+					getline(cin, e);
+					if (e == "O sa fie lata") {
+						cout << "Bravo, continua" << endl;
+						getline(cin, f);
+						if (f == "Suntem in brigada") {
+							cout << "Bravo, continua" << endl;
+							getline(cin, g);
+							if (g == "Atmosfera e ciudata") {
+								cout << "Bravo, continua" << endl;
+								getline(cin, h);
+								if (h == "Cand bei apa plata") {
+									cout << "Bravo, continua" << endl;
+									getline(cin, i);
+									if (i == "Da ceva pe gat daca'o duci mult") {
+										cout << "Bravo, continua" << endl;
+										getline(cin, j);
+										if (j == "Hai hai hai hai sunt de la inceput") {
+											cout << "Bravo, continua" << endl;
+											getline(cin, k);
+											if (j == "Te rog mult sa nu faci urat") {
+												cout << "Bravo, continua" << endl;
+												getline(cin, l);
+												if (l == "Ca ii sperii si dupaia fug") {
+													cout << "Bravo, continua" << endl;
+													getline(cin, m);
+													if (m == "Cauta in agenda") {
+														cout << "Bravo, continua" << endl;
+														getline(cin, n);
+														if (n == "O studenta") {
+															cout << "Bravo, continua" << endl;
+															getline(cin, o);
+															if (o == "Daca e indecenta e perfecta") {
+																cout << "Bravo, continua" << endl;
+																getline(cin, p);
+																if (p == "Daca nu e buna chiar si o eleva") {
+																	cout << "Bravo, continua" << endl;
+																	getline(cin, A);
+																	if (A == "Nu eminenta") {
+																		cout << "Bravo, continua" << endl;
+																		getline(cin, B);
+																		if (B == "Cat mai repetenta") {
+																			cout << "Bravo, continua" << endl;
+																			getline(cin, C);
+																			if (C == "Fresh nu'i bine delok") {
+																				cout << "Bravo, continua" << endl;
+																				getline(cin, D);
+																				if (D == "Da'mi ceva tare ca numai suport") {
+																					cout << "Bravo, continua" << endl;
+																					getline(cin, E);
+																					if (E == "Am cash") {
+																						cout << "Bravo, continua" << endl;
+																						getline(cin, F);
+																						if (F == "Si vreau sa ma imbat") {
+																							cout << "Bravo, continua" << endl;
+																							getline(cin, G);
+																							if (G == "Stii ca in seara asta o sa gust din tot") {
+																								cout << " BRAVOOOOOOOOOOOOOOOOOOOOOOO";
+																							}
+																						}
+																						else {
+																							cout << " GRESITTTTTTTTTTT";
+																						}
+
+																					}
+																					else {
+																						cout << " GRESITTTTTTTTTTT";
+																					}
+																				}
+																				else {
+																					cout << " GRESITTTTTTTTTTT";
+																				}
+																			}
+																			else {
+																				cout << " GRESITTTTTTTTTTT";
+																			}
+																		}
+																		else {
+																			cout << " GRESITTTTTTTTTTT";
+																		}
+																	}
+																	else {
+																		cout << " GRESITTTTTTTTTTT";
+																	}
+																}
+																else {
+																	cout << " GRESITTTTTTTTTTT";
+																}
+															}
+															else {
+																cout << " GRESITTTTTTTTTTT";
+															}
+														}
+														else {
+															cout << " GRESITTTTTTTTTTT";
+														}
+													}
+													else {
+														cout << " GRESITTTTTTTTTTT";
+													}
+												}
+												else {
+													cout << " GRESITTTTTTTTTTT";
+												}
+											}
+											else {
+												cout << " GRESITTTTTTTTTTT";
+											}
+										}
+										else {
+											cout << " GRESITTTTTTTTTTT";
+										}
+									}
+									else {
+										cout << " GRESITTTTTTTTTTT";
+									}
+								}
+								else {
+									cout << " GRESITTTTTTTTTTT";
+								}
+							}
+							else {
+								cout << " GRESITTTTTTTTTTT";
+							}
+						}
+						else {
+							cout << " GRESITTTTTTTTTTT";
+
+						}
+					}
+					else {
+						cout << " GRESITTTTTTTTTTT";
+					}
+
+				}
+				else {
+					cout << " GRESITTTTTTTTTTT";
+				}
+			}
+			else {
+				cout << " GRESITTTTTTTTTTT";
+			}
+		}
+
+		else {
+			cout << " GRESITTTTTTTTTTT";
+		}
+	}
+	else {
+
+		cout << " GRESITTTTTTTTTTT";
+	}
+
+
+	return 0;
+}
